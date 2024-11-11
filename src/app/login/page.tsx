9994a9a2940cdccf7c049cc94d873b3b1f3710ea
Shun <saitoshin2145@gmail.com>
2024-11-11 10:55:51 +0900

    issue-03: added the login logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,12 @@
-'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { FormEvent } from 'react';
-import './style.css';
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
+import "./style.css";
 
 export default function Login() {
-  const [email, setEmail] = useState<any>('');
-  const [password, setPassword] = useState<any>('');
+  const [email, setEmail] = useState<any>("");
+  const [password, setPassword] = useState<any>("");
 
   const router = useRouter();
   const submitForm = async (event: FormEvent<HTMLFormElement>) => {
@@ -17,34 +17,36 @@ export default function Login() {
   };
   return (
     <div>
-      <div id='loginContainer'>
+      <div id="loginContainer">
         <h2>
-          <p className='subHeader'>LOGIN</p>
-          <p className='mainHeader'>管理画面ログイン</p>
+          <p className="subHeader">LOGIN</p>
+          <p className="mainHeader">管理画面ログイン</p>
         </h2>
-        <form id='loginForm' onSubmit={submitForm}>
-          <label htmlFor='email' className='formHeader'>
+        <form id="loginForm" action="/api/login" method="POST">
+          <label htmlFor="email" className="formHeader">
             email
           </label>
           <br />
           <input
-            type='text'
-            id='email'
-            name='email'
-            onChange={(e: any) => setEmail(e.target.value)}></input>
+            type="text"
+            id="email"
+            name="email"
+            onChange={(e: any) => setEmail(e.target.value)}
+          ></input>
           <br />
-          <label htmlFor='password' className='formHeader'>
+          <label htmlFor="password" className="formHeader">
             パスワード
           </label>
           <br />
           <input
-            type='password'
-            id='password'
-            name='password'
-            onChange={(e: any) => setPassword(e.target.value)}></input>
+            type="password"
+            id="password"
+            name="password"
+            onChange={(e: any) => setPassword(e.target.value)}
+          ></input>
           <br />
-          <div style={{ textAlign: 'center' }}>
-            <button type='submit' className='loginSubmit'>
+          <div style={{ textAlign: "center" }}>
+            <button type="submit" className="loginSubmit">
               Submit
             </button>
           </div>
