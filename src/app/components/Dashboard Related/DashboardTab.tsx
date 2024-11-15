a620e6fade55e88a2bf2027266007791283c2b12
Shun <saitoshin2145@gmail.com>
2024-11-16 08:33:16 +0900

    issue-06: created dashboard input

--- src/app/components/Dashboard Related/DashboardTab.tsx
+++ src/app/components/Dashboard Related/DashboardTab.tsx
@@ -1,9 +1,49 @@
+'use client';
+import React, { useState } from 'react';
 import './style.css';
 
-export const DashboardTab = () => {
+type tabInputs = {
+  tabs: string[];
+};
+export const DashboardTab = ({ tabs }: tabInputs) => {
+  const [currentTab, setCurrentTab] = useState(tabs[0]);
+
+  const handleCurrentTab = (e: string) => {
+    setCurrentTab(e);
+  };
   return (
     <div className='tab'>
-      <div></div>
+      <div className='tabMenu'>
+        <h5>管理者メニュー</h5>
+        {tabs.map((item) => (
+          <button
+            key={item}
+            className='tabButton'
+            onClick={() => {
+              handleCurrentTab(item);
+            }}>
+            {item}
+          </button>
+        ))}
+      </div>
+      <div className='tabBody'>
+        <div className='tabContainer'>
+          <h5>{currentTab}</h5>
+
+          <table id='tabTable'>
+            <tr>
+              <th>タイトル</th>
+              <th>サムネ画像</th>
+              <th>説明文</th>
+              <th>作成日</th>
+              <th>最新更新日</th>
+              <th>公開・非公開</th>
+              <th>編集</th>
+            </tr>
+            <tr></tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import './style.css';
+import { DashboardTab } from '../components/Dashboard Related/DashboardTab';
 export default function Dashboard() {
+  const tabs = ['サービス', 'ブログ', 'ユーザー'];
   return (
     <div>
       <div id='dashboardContainer'>
@@ -8,6 +10,7 @@ export default function Dashboard() {
           <p className='subHeader'>DASHBOARD</p>
           <p className='mainHeader'>管理画面</p>
         </h2>
+        <DashboardTab tabs={tabs} />
       </div>
     </div>
   );
