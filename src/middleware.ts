5765df765fb83d10f048bf84d9854af34a125704
Shun <saitoshin2145@gmail.com>
2024-11-16 17:46:36 +0900

    issue-07: add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
 export async function middleware(req: NextRequest) {
-  console.log(req.headers);
+  console.log(req.headers.get('Authorization'));
   if (req.nextUrl.pathname.match('/api/post-blog')) {
-    const authroizationHeader = req.headers.get('authorization');
-    if (authroizationHeader) {
+    const authroizationHeader = req.headers.get('Authorization');
+    if (!authroizationHeader) {
       return NextResponse.json({ message: 'Please Log In' }, { status: 403 });
     }
     //const token = req.headers.authorization.split('')[1];
@@ -14,12 +14,19 @@ export async function middleware(req: NextRequest) {
       return NextResponse.json({ message: 'Please Log In' }, { status: 403 });
     }
     const secretKey = new TextEncoder().encode(process.env.SESSION_SECRET!);
-
-    let success = await jwtVerify(token, secretKey);
-    if (!success) {
+    const options = {
+      algorithms: ['HS256'],
+    };
+    let decoded = await jwtVerify(token, secretKey, options);
+    console.log(decoded);
+    if (!decoded) {
       return NextResponse.redirect(new URL('/login'));
     }
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: '/api/post-blog',
+};
