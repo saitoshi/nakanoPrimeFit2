fdb9928be47d2b1d4ac8363dd7211a364b9916be
Shun <saitoshin2145@gmail.com>
2024-11-18 07:46:58 +0900

    issue-07: blog feature

--- src/app/api/post-blog/route.ts
+++ src/app/api/post-blog/route.ts
@@ -1,10 +1,25 @@
 import connectDB from '@/app/utils/connectDB';
 import { BlogModel } from '@/app/utils/dataSchemas/blogSchema';
 import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   try {
     await connectDB();
+    let token = request.headers.get('Authorization');
+    if (!token) {
+      return NextResponse.json(
+        { message: 'Error No Token was received', status: 403 },
+        { status: 201 },
+      );
+    }
+    const secretKey = process.env.SESSION_SECRET as string;
+
+    let decoded = await jwt.verify(token, secretKey);
+
+    if (!decoded) {
+      return NextResponse.json({ message: 'verification error', statu: 403 });
+    }
     let reqBody = await request.json();
     let {
       title,
