0a252b5bf2dc02d093258050bfbcef04e29fc913
Shun <saitoshin2145@gmail.com>
2024-11-24 14:31:38 +0900

    issue-09: modified the user data form

--- src/app/components/FormRelated/UserForm.tsx
+++ src/app/components/FormRelated/UserForm.tsx
@@ -6,14 +6,39 @@ import './style.css';
 
 export const UserForm = () => {
   //variable declaration using the userState hook
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState<IUser['email']>('');
+  const [password, setPassword] = useState<IUser['password']>('');
   const [role, setRole] = useState<IUser['role']>('');
   // variable for the conditional rendering
   const [load, isLoad] = useState<boolean>(false);
   const [error, isError] = useState<boolean>(false);
   const [errorMsg, setErrorMsg] = useState<string>('');
   const [success, setSuccess] = useState<boolean>(false);
+  const [filled, setFilled] = useState<boolean>(false);
+  //check if the form is filled
+  useEffect(() => {
+    if (email && password && role) setFilled(true);
+  }, [email, password, role]);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    isLoad(true);
+    try {
+      console.log({ email, password, role });
+      let regEmail =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!regEmail.test(email)) {
+        isLoad(false);
+        isError(true);
+        setErrorMsg('有効なメールアドレスを入力してください。');
+      }
+      isLoad(false);
+      setSuccess(true);
+    } catch (error) {
+      isLoad(false);
+      isError(true);
+      setErrorMsg('エラーが発生いたしました。後ほど、もう一度試してください');
+    }
+  };
   if (load) {
     return <LoadingWheel />;
   }
@@ -23,7 +48,7 @@ export const UserForm = () => {
         <p className='subHeader'>ADD USER</p>
         <p className='mainHeader'>管理ユーザーを追加する</p>
       </h2>
-      <form className='formCreation'>
+      <form className='formCreation' onSubmit={handleSubmit}>
         <label htmlFor='email' className='formHeader'>
           ユーザーのメールアドレス
         </label>
@@ -43,7 +68,6 @@ export const UserForm = () => {
           type='password'
           id='password'
           name='password'
-          placeholder='yamato.takeshi@gmail.com'
           onChange={(e: any) => {
             setPassword(e.target.value);
           }}></input>
@@ -62,8 +86,11 @@ export const UserForm = () => {
           <option value='writer'>ライター</option>
         </select>
         <div style={{ textAlign: 'center' }}>
-          <button id='submitButton'>ユーザー登録</button>
+          <button type='submit' id='submitButton' disabled={!filled}>
+            ユーザー登録
+          </button>
         </div>
+        {error ? <span className='errorMsg'>{errorMsg}</span> : <></>}
       </form>
     </div>
   );
