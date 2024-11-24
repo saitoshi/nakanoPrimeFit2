63ec3011b3d5503bf4b6e4a54dc86f4e20d791c5
Shun <saitoshin2145@gmail.com>
2024-11-24 16:40:04 +0900

    issue-10: dashboard page

--- src/app/components/Dashboard Related/DashboardTab.tsx
+++ src/app/components/Dashboard Related/DashboardTab.tsx
@@ -3,7 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { LoadingWheel } from '../ConditionalComponents/LoadingWheel';
 import { getBlogs } from '@/app/utils/assistFunctions/blogFunction';
 import { IBlog } from '@/app/constants/type';
+import { BlogForm } from '../FormRelated/BlogForm';
+import { UserForm } from '../FormRelated/UserForm';
 import './style.css';
+import { useMedia } from 'react-use';
 type tabInputs = {
   tabs: string[];
 };
@@ -90,5 +93,114 @@ export const DashboardTab = ({ tabs }: tabInputs) => {
         </div>
       </div>
     </div>
+  );
+};
+
+export const DashboardMenu = () => {
+  const isDesktop = useMedia('(min-width: 650px)', true);
+  const [openBlog, setOpenBlog] = useState(true);
+  const [openService, setOpenService] = useState<boolean>(false);
+  const [openUser, setOpenUser] = useState<boolean>(false);
+  const [existBlog, setExistBlog] = useState<boolean>(false);
+  const [existUser, setExistUser] = useState<boolean>(false);
+  const [existService, setExistService] = useState<boolean>(false);
+  const tabs = ['サービス', 'ブログ', 'ユーザー'];
+  return (
+    <>
+      {isDesktop ? (
+        <div className='tab'>
+          <div className='tabMenu'>
+            <button className='tabButton'>サービス</button>
+            <button
+              className='createBlogButton'
+              onClick={async () => {
+                await setOpenBlog(true);
+                await setOpenService(false);
+                await setOpenUser(false);
+              }}>
+              &#x270E; ブログ作成
+            </button>
+            <button
+              className='addServiceButton'
+              onClick={async () => {
+                await setOpenBlog(false);
+                await setOpenService(true);
+                await setOpenUser(false);
+              }}>
+              &#x271A; サービス追加
+            </button>
+            <button
+              className='addUserButton'
+              onClick={async () => {
+                await setOpenBlog(false);
+                await setOpenService(false);
+                await setOpenUser(true);
+              }}>
+              &#x263A; 新規ユーザー追加
+            </button>
+          </div>
+          <div className='tabBody'>
+            <div
+              className='tabContainer'
+              id='blogTabBody'
+              style={openBlog ? { display: 'block' } : { display: 'none' }}>
+              <BlogForm />
+            </div>
+            <div
+              className='tabContainer'
+              id='userTabBody'
+              style={openUser ? { display: 'block' } : { display: 'none' }}>
+              <UserForm />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div id='mobileTab'>
+          <div className='mobileTabHeader'>
+            <button
+              className='createBlogButton'
+              onClick={async () => {
+                await setOpenBlog(true);
+                await setOpenService(false);
+                await setOpenUser(false);
+              }}>
+              &#x270E; ブログ作成
+            </button>
+            <button
+              className='addServiceButton'
+              onClick={async () => {
+                await setOpenBlog(false);
+                await setOpenService(true);
+                await setOpenUser(false);
+              }}>
+              &#x271A; サービス追加
+            </button>
+            <button
+              className='addUserButton'
+              onClick={async () => {
+                await setOpenBlog(false);
+                await setOpenService(false);
+                await setOpenUser(true);
+              }}>
+              &#x263A; 新規ユーザー追加
+            </button>
+          </div>
+          <div
+            className='mobileTabBody'
+            style={openBlog ? { display: 'block' } : { display: 'none' }}>
+            <div className='tabContainer'>
+              <BlogForm />
+            </div>
+          </div>
+          <div
+            className='mobileTabBody'
+            style={openUser ? { display: 'block' } : { display: 'none' }}>
+            <div className='tabContainer'>
+              <UserForm />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
