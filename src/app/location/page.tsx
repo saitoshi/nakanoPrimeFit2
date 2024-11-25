136f5d46d78662e003708a5caa745a76745a21f9
Shun <saitoshin2145@gmail.com>
2024-11-25 11:49:56 +0900

    dev - modified the location page

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -4,6 +4,10 @@ export default function Location() {
   return (
     <div>
       <div className='pageContainer'>
+        <h2>
+          <p className='subHeader'>LOCATION</p>
+          <p className='mainHeader'>店舗情報</p>
+        </h2>
         <LocationCard />
       </div>
     </div>
