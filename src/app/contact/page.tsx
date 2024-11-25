bafa232a6f89a6a9117b8b3de9d4dfa04d6ee822
Shun <saitoshin2145@gmail.com>
2024-11-25 11:55:07 +0900

    issue-12-added the contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+export default function Contact() {
+  return (
+    <div className='pageContainer'>
+      <h2>
+        <p className='subHeader'>CONTACT</p>
+        <p className='mainHeader'>お問い合わせ</p>
+      </h2>
+      <p>
+        弊社へのお問い合わせはライン経由でお願いしております。
+        <br />
+        登録は以下のボタンでお願いいたします。
+      </p>
+    </div>
+  );
+}
