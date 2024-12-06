217292a358b2f12eb5b9e0049cf30812625f9040
Shun <saitoshin2145@gmail.com>
2024-12-06 09:50:56 +0900

    issue-20: update the eslitn error

--- src/app/utils/connectDB.ts
+++ src/app/utils/connectDB.ts
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_API_KEY!);
   } catch (error) {
-    console.log('Failure: Cannot connect to MongoDB');
+    console.log('Failure: Cannot connect to MongoDB' + error);
     throw new Error();
   }
 };
